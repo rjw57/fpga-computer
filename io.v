46150ef4f2e35ca4e10395a52558a6e96a568d6b
Rich Wareham <rich@richwareham.com>
2018-10-04 15:47:20 +0100

    add second 32K RAM bank
    Total RAM is now 64K but top 2K is write-only.

--- io.v
+++ io.v
@@ -9,16 +9,19 @@ module io(
   output reg [7:0] io_port
 );
 
-wire ram_select = (addr[15] == 1'b0) ? 1'b1 : 1'b0;
+wire ram_1_select = (addr[15] == 1'b0) ? 1'b1 : 1'b0;
+wire ram_2_select = (addr[15] == 1'b1) ? 1'b1 : 1'b0;
 wire rom_select = (addr[15:11] == 5'b11111) ? 1'b1 : 1'b0;
 wire io_port_select = (addr== 16'h8400) ? 1'b1 : 1'b0;
 
-reg ram_select_reg, rom_select_reg, io_port_select_reg;
+reg ram_1_select_reg, ram_2_select_reg, rom_select_reg, io_port_select_reg;
 assign data_out = rom_select_reg ? rom_data_out : 8'bZ;
-assign data_out = ram_select_reg ? ram_data_out : 8'bZ;
+assign data_out = ram_1_select_reg ? ram_1_data_out : 8'bZ;
+assign data_out = (ram_2_select_reg && ~rom_select_reg) ? ram_2_data_out : 8'bZ;
 always @(posedge clk)
 begin
-  ram_select_reg <= ram_select;
+  ram_1_select_reg <= ram_1_select;
+  ram_2_select_reg <= ram_2_select;
   rom_select_reg <= rom_select;
   io_port_select_reg <= io_port_select;
 end
@@ -30,13 +33,22 @@ bootrom bootrom(
   .data(rom_data_out)
 );
 
-wire [7:0] ram_data_out;
-ram ram(
+wire [7:0] ram_1_data_out;
+ram ram_1(
   .clk(clk),
   .addr(addr[14:0]),
   .data_in(data_in),
-  .data_out(ram_data_out),
-  .write_enable(write_enable && ram_select)
+  .data_out(ram_1_data_out),
+  .write_enable(write_enable && ram_1_select)
+);
+
+wire [7:0] ram_2_data_out;
+ram ram_2(
+  .clk(clk),
+  .addr(addr[14:0]),
+  .data_in(data_in),
+  .data_out(ram_2_data_out),
+  .write_enable(write_enable && ram_2_select)
 );
 
 always @(posedge clk)
