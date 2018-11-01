39d503fa1cf2d67d1501b0256325f36ff3d98a46
Rich Wareham <rich@richwareham.com>
2018-11-01 21:08:20 +0000

    tidy up code and enable VRAM

--- computer.v
+++ computer.v
@@ -20,9 +20,11 @@ wire cpu_writing;
 wire [7:0] rom_data;
 wire [7:0] ram_data;
 
-reg [15:0] select_addr;
-always @(negedge cpu_clk) select_addr <= cpu_addr;
-assign cpu_data_in = (select_addr[15:11] == 5'b11111) ? rom_data : ram_data;
+// Register CPU address bus o negative edge of CPU clock so that select lines
+// remain valid into next cycle.
+reg [15:0] addr_reg;
+always @(negedge cpu_clk) addr_reg <= cpu_addr;
+assign cpu_data_in = (addr_reg[15:11] == 5'b11111) ? rom_data : ram_data;
 
 // Reset line. We tie it to the CPU clock as the slowest clock in the system.
 reset_timer reset_timer(.clk(cpu_clk), .reset(reset));
@@ -37,31 +39,16 @@ begin
   //io_port <= cpu_addr[15:8];
 end
 
-/*
-wire cpu_rw;
-assign cpu_writing = ~cpu_rw;
+// Form a derived CPU clock which is one dot clock delayed. We need this because
+// the data in to the CPU needs to be updated just *after* the positive edge for
+// the following cycle.
+reg cpu_clk_delay;
+always @(posedge dot_clk) cpu_clk_delay <= cpu_clk;
 
-bc6502 cpu(
-  .reset(reset),
-  .clk(cpu_clk),
-
-  .nmi(1'b0),
-  .irq(1'b0),
-  .rdy(1'b1),
-  .so(1'b0),
-
-  .ma(cpu_addr),
-  .rw(cpu_rw),
-  .di(cpu_data_in),
-  .do(cpu_data_out)
-);
-*/
-
-// HACK!
-reg [7:0] di1;
-reg cpu_clk_y;
-always @(posedge dot_clk) cpu_clk_y <= cpu_clk;
-always @(posedge cpu_clk_y) di1 <= cpu_data_in;
+// The data in lines to the CPU are latched just after the positive CPU clock
+// edge. They related to the preceding cycle's address.
+reg [7:0] cpu_data_in_reg;
+always @(posedge cpu_clk_delay) cpu_data_in_reg <= cpu_data_in;
 
 cpu_65c02 cpu(
   .reset(reset),
@@ -73,7 +60,7 @@ cpu_65c02 cpu(
 
   .AB(cpu_addr),
   .WE(cpu_writing),
-  .DI(di1),
+  .DI(cpu_data_in_reg),
   .DO(cpu_data_out)
 );
 
@@ -95,17 +82,8 @@ vram vram(
 
   .cpu_addr(cpu_addr),
   .cpu_data_in(cpu_data_out),
-//  .cpu_data_out(ram_data),
-//  .cpu_write_enable(cpu_writing)
-  .cpu_write_enable(1'b0)
-);
-
-spram32k8 ram(
-  .clk(~cpu_clk),
-  .addr(cpu_addr[14:0]),
-  .write_enable(cpu_writing && ~cpu_addr[15]),
-  .data_in(cpu_data_out),
-  .data_out(ram_data)
+  .cpu_data_out(ram_data),
+  .cpu_write_enable(cpu_writing)
 );
 
 endmodule
